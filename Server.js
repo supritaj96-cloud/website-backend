7753bc856acdfb3239cac7c2efd9e8e0b021776c
supritaj96-cloud <supritaj96@gmail.com>
2026-06-26 19:56:02 +0530

    fix railway server port binding

--- Server.js
+++ Server.js
@@ -32,7 +32,7 @@ const startServer = async () => {
         await db.initialize();
         await db.sequelize.sync({ alter: true });
 
-        app.listen(PORT, () => {
+        app.listen(PORT, "0.0.0.0",() => {
             console.log(`Server is running on port ${PORT}`);
         });
     } catch (error) {
