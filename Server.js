623fd0747bc0707bf7b91f47e51ba17152b19527
supritaj96-cloud <supritaj96@gmail.com>
2026-06-26 20:16:31 +0530

    message

--- Server.js
+++ Server.js
@@ -12,7 +12,7 @@ const orderRoutes = require('./Routes/OrderRoutes');
 const paymentRoutes = require('./Routes/PaymentRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
@@ -32,7 +32,7 @@ const startServer = async () => {
         await db.initialize();
         await db.sequelize.sync({ alter: true });
 
-        app.listen(PORT, "0.0.0.0",() => {
+        app.listen(PORT, "0.0.0.0", () => {
             console.log(`Server is running on port ${PORT}`);
         });
     } catch (error) {
